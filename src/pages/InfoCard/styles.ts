90cbf6201298f8b1cd567a8f266d8978740ba758
Lucas Gomes <lucasglpes@gmail.com>
2021-04-30 21:40:05 -0300

    Fixing AboutPage

--- src/pages/InfoCard/styles.ts
+++ src/pages/InfoCard/styles.ts
@@ -3,22 +3,28 @@ import styled from 'styled-components';
 export const Div = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: space-around;
     align-items: flex-start;
 
     width: 22rem;
-    height: 8rem;
+    height: 9rem;
 
     font-family: Roboto, sans-serif;
 
-    color: #000;
+    color: #142634;
 
     h2{
         font-size: 1.25rem;
-        margin-bottom: 0px;
     }
+
     span{
-        margin-top: 5px;
         font-size: 1rem;
     }
+
+    img{
+        height:2.5rem;
+        width: 3rem;
+    }
+
+
 `;
