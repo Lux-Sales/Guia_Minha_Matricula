8a97216957b67f28ac3406a9c70def7772c4a271
Gabriel <gabriel@JoaoGilberto.localdomain>
2021-05-06 20:09:14 -0300

    pagina para alterar o apelido

---
+++ src/pages/ConfigurationNicknamePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NickManeCard from '../NicknameCard';
+import { ConfigurationNicknamePageStyle } from './style';
+
+const ConfigurationNicknamePage : React.FC = () => (
+
+  <ConfigurationNicknamePageStyle>
+    <NickManeCard />
+  </ConfigurationNicknamePageStyle>
+);
+
+export default ConfigurationNicknamePage;
---
+++ src/pages/ConfigurationNicknamePage/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import ConfigurationPage from '../ConfigurationPage';
+
+export const ConfigurationNicknamePageStyle = styled(ConfigurationPage)`
+    div {
+        background-color: red;
+    }
+`;
