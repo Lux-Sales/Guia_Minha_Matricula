23d2cb9f786ca07c550653610531cb724e9b8312
Gabriel <gabriel@JoaoGilberto.localdomain>
2021-05-06 19:52:59 -0300

    adicionando rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,9 @@ import HomePage from '../pages/HomePage';
 import AboutPage from '../pages/AboutPage';
 import LoginPage from '../pages/LoginPage';
 import UserHome from '../pages/UserHome';
+import ConfigurationPasswordPage from '../pages/ConfigurationPasswordPage';
+import ConfigurationDeletePage from '../pages/ConfigurationDeletePage';
+import ConfigurationNicknamePage from '../pages/ConfigurationNicknamePage';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -11,6 +14,9 @@ const Routes: React.FC = () => (
     <Route exact path="/sobre" component={AboutPage} />
     <Route exact path="/login" component={LoginPage} />
     <Route exact path="/userhome" component={UserHome} />
+    <Route exact path="/Configuration-Password" component={ConfigurationPasswordPage} />
+    <Route exact path="/Configuration-DeleteCont" component={ConfigurationDeletePage} />
+    <Route exact path="/Configuration-NickName" component={ConfigurationNicknamePage} />
   </Switch>
 );
 
