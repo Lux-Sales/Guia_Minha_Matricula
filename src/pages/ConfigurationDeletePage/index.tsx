edab54566d495ea8a174ccad73c05bab00d9b259
Gabriel <gabriel@JoaoGilberto.localdomain>
2021-05-06 20:07:17 -0300

    pagina para deletar conta

---
+++ src/pages/ConfigurationDeletePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DeleteCard from '../DeleteCard';
+import { ConfigurationPageStyle } from './styles';
+
+const ConfigurationPasswordPage : React.FC = () => (
+
+  <ConfigurationPageStyle>
+    <DeleteCard />
+  </ConfigurationPageStyle>
+);
+
+export default ConfigurationPasswordPage;
---
+++ src/pages/ConfigurationDeletePage/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import ConfigurationPage from '../ConfigurationPage';
+
+export const ConfigurationPageStyle = styled(ConfigurationPage)`
+    background-color: red;
+`;
